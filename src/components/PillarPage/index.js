c5b0ba2dcc454227b12d9de5645269d41b2961a1
Dev-Shai <shai.dev.code@gmail.com>
2023-03-22 20:51:33 +0000

    Created file structure and installed bootstrap.

---
+++ src/components/PillarPage/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from "./reportWebVitals";
+
+export default
+//INSERT NAME HERE;
---
+++ src/utils/API-Quote.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+// Export an object containing methods we'll use for accessing the Dog.Ceo API
+
+export default {
+};
