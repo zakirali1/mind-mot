0a3e4bd1f6047b798f714b6470b1eb6d540aa495
Lola <86410482+19Lola89@users.noreply.github.com>
2023-03-24 12:19:00 +0000

    added nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Relax from './pages/Relax';
-import Eat from './pages/Eat';
-import Move from './pages/Move';
-import Sleep from './pages/Sleep';
-import Home from './pages/Home';
-import About from './pages/About';
-import Navbar from './components/Navbar';
-import Wrapper from './components/Wrapper';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Relax from "./pages/Relax";
+import Eat from "./pages/Eat";
+import Move from "./pages/Move";
+import Sleep from "./pages/Sleep";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
@@ -16,12 +16,12 @@ function App() {
         <Navbar />
         <Wrapper>
           <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/relax" element={<Relax/>} />
-            <Route path="/eat" element={<Eat/>} />
-            <Route path="/move" element={<Move/>} />
-            <Route path="/sleep" element={<Sleep/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/relax" element={<Relax />} />
+            <Route path="/eat" element={<Eat />} />
+            <Route path="/move" element={<Move />} />
+            <Route path="/sleep" element={<Sleep />} />
           </Routes>
         </Wrapper>
       </div>
