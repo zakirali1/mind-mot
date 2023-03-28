c8c63e6df15262cfba439b1faf46abf145fc25e9
Lola <86410482+19Lola89@users.noreply.github.com>
2023-03-28 01:14:37 +0100

    gratitude input amended

--- src/pages/Relax.js
+++ src/pages/Relax.js
@@ -4,46 +4,42 @@ import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
 import heroBackground from "../assets/hero-background.jpg";
-import Card from 'react-bootstrap/Card'
+import Card from "react-bootstrap/Card";
+import "./relax.css";
 
 function Relax() {
+  const inputs = JSON.parse(localStorage.getItem("gratitude"));
+  console.log(inputs.length);
 
-  const inputs = JSON.parse(localStorage.getItem("gratitude"))
-  console.log(inputs.length)
+  const Momento = inputs.map((input) => (
+    <>
+      <Card className="journalCards">
+        <Card.Body>
+          <Card.Title>{input.input1}</Card.Title>
+        </Card.Body>
+      </Card>
+      <Card className="journalCards">
+        <Card.Body>
+          <Card.Title>{input.input2}</Card.Title>
+        </Card.Body>
+      </Card>
+      <Card className="journalCards">
+        <Card.Body>
+          <Card.Title>{input.input3}</Card.Title>
+        </Card.Body>
+      </Card>
+    </>
+  ));
 
- 
- const Momento = inputs.map((input) => (
-  <>
-  <Card style={{ width: '18rem' }}>
-  <Card.Body>
-    <Card.Title>{input.input1}</Card.Title>
-  </Card.Body>
-</Card>
-<Card style={{ width: '18rem' }}>
-  <Card.Body>
-    <Card.Title>{input.input2}</Card.Title>
-  </Card.Body>
-</Card>
-<Card style={{ width: '18rem' }}>
-  <Card.Body>
-    <Card.Title>{input.input3}</Card.Title>
-  </Card.Body>
-</Card>
-</>
-
- ))
-
-  
   return (
     <>
-    <div className="mainContainer">
-      <MiniHero backgroundImage={heroBackground}>
-        <h1>Relax</h1>
-      </MiniHero>
-      <Container style={{ marginTop: 30 }}>
-      </Container>
-    </div>
-    {Momento}
+      <div className="mainContainer">
+        <MiniHero backgroundImage={heroBackground}>
+          <h1>Relax</h1>
+        </MiniHero>
+        <Container style={{ marginTop: 30 }}></Container>
+      </div>
+      {Momento}
     </>
   );
 }
