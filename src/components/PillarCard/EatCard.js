56500650d32b3dc8b72cec7cec15ec2a25907b6e
Lola <86410482+19Lola89@users.noreply.github.com>
2023-03-30 13:12:58 +0300

    resloved warnings

--- src/components/PillarCard/EatCard.js
+++ src/components/PillarCard/EatCard.js
@@ -1,86 +1,98 @@
-import React, { useState, useEffect } from 'react'
-import pillarData from "../pillarData.json"
-import GaugeChart from 'react-gauge-chart';
+import React, { useState, useEffect } from "react";
+import pillarData from "../pillarData.json";
+import GaugeChart from "react-gauge-chart";
 
-{/* UseState to track  changes for clicked items, useState to monitor for any changes */}
+// UseState to track  changes for clicked items, useState to monitor for any changes
 
 function EatCard() {
+  const [eatScore, setEatScore] = useState(
+    JSON.parse(localStorage.getItem("EatScore")) || []
+  );
 
-    const [eatScore, setEatScore] = useState(JSON.parse(localStorage.getItem("EatScore")) || []);
+  const [eatPercent, setEatPercent] = useState();
 
-    const [eatPercent, setEatPercent] = useState()
+  const eatPillar = pillarData[0];
 
-    const eatPillar = pillarData[0]
+  //   const savedEatScore = JSON.parse(localStorage.getItem("EatScore")) || [];
 
-    const savedEatScore = JSON.parse(localStorage.getItem("EatScore")) || [];
+  // conditional render based on ischecked being true
 
-    {/* conditional render based on ischecked being true*/}
+  function handleCheckedBox(e) {
+    console.log(e);
+    const currentVal = e.target.value;
+    const isChecked = e.target.checked;
 
-    function handleCheckedBox(e) {
-        console.log(e)
-        const currentVal = e.target.value
-        const isChecked = e.target.checked;
-
-        if(isChecked) {
-            setEatScore([...eatScore, currentVal ])
-        
-
-        } else {
-            setEatScore(eatScore.filter(val => val !== currentVal))
-        }
-        
-
+    if (isChecked) {
+      setEatScore([...eatScore, currentVal]);
+    } else {
+      setEatScore(eatScore.filter((val) => val !== currentVal));
     }
+  }
 
-    useEffect(() =>{
-        
-        localStorage.setItem("EatScore", JSON.stringify(eatScore));
-        const updatedVal = eatScore.length / eatPillar.toDo.list.length;
-        setEatPercent(updatedVal)
+  useEffect(() => {
+    localStorage.setItem("EatScore", JSON.stringify(eatScore));
+    const updatedVal = eatScore.length / eatPillar.toDo.list.length;
+    setEatPercent(updatedVal);
+  }, [eatScore, eatPillar.toDo.list.length]);
 
-    },[eatScore])
+  console.log(eatPercent);
 
-    console.log(eatPercent)
+  const listItems = eatPillar.toDo.list.map((item, index) => (
+    <div key={index}>
+      <div className="form-check">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          onChange={handleCheckedBox}
+          value={item}
+          checked={eatScore.includes(item)}
+        />
+        <label className="form-check-label" htmlFor="flexCheckDefault">
+          {item}
+        </label>
+      </div>
+    </div>
+  ));
 
-    const listItems = eatPillar.toDo.list.map((item, index) =>  (
-        <div key = {index}>
-            <div className="form-check">
-                <input className="form-check-input" type="checkbox" onChange={handleCheckedBox} value={item} checked={eatScore.includes(item)}  />
-                <label className="form-check-label" htmlFor="flexCheckDefault">
-                    {item}
-                </label>
-            </div>
-        </div>
-    ))
+  return (
+    <div className="container col-lg-3 col-md-6 col-sm-12 p-1 pillarContainer">
+      <div className="card animate__animated animate__fadeInUp h-100 pillarCard">
+        <div className="card-body">
+          <h4 className="card-title text-center pillarCard-title">
+            {eatPillar.pillarName}
+          </h4>
 
-    return (
-        
-        <div className="container col-lg-3 col-md-6 col-sm-12 p-1 pillarContainer">
-        <div className="card animate__animated animate__fadeInUp h-100 pillarCard">
-          <div className="card-body">
-            <h4 className="card-title text-center pillarCard-title">{eatPillar.pillarName}</h4>
-  
-              <GaugeChart id="pillar-gauge" 
-              nrOfLevels={20} 
-              arcsLength={[0.2, 0.5, 0.3]}
-              colors={['rgba(171, 13, 13, 0.8)','rgba(255, 220, 0, 0.8)', 'rgba(11, 150, 11, 0.8)']}
-              percent={eatPercent} 
-              needleColor="rgba(48, 50, 78, 0.7)"
-              textColor='rgb(50, 69, 32)'
-              animateDuration='4000'
+          <GaugeChart
+            id="pillar-gauge"
+            nrOfLevels={20}
+            arcsLength={[0.2, 0.5, 0.3]}
+            colors={[
+              "rgba(171, 13, 13, 0.8)",
+              "rgba(255, 220, 0, 0.8)",
+              "rgba(11, 150, 11, 0.8)",
+            ]}
+            percent={eatPercent}
+            needleColor="rgba(48, 50, 78, 0.7)"
+            textColor="rgb(50, 69, 32)"
+            animateDuration="4000"
             // change the percent value inside the curly braces to change the gauge chart -this is where we need JS to calculate the number of todos done
-              />
-  
-            {/* Render Listitems */}
-            {listItems}
-            
-            
-            <a href={'/eat'} target= '_blank' className="card-link">More info</a> 
-          </div>
+          />
+
+          {/* Render Listitems */}
+          {listItems}
+
+          <a
+            href={"/eat"}
+            target="_blank"
+            className="card-link"
+            rel="noreferrer"
+          >
+            More info
+          </a>
         </div>
-        </div>
-       
-    )
+      </div>
+    </div>
+  );
 }
 
-export default EatCard
+export default EatCard;
--- src/pages/Eat.js
+++ src/pages/Eat.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import MiniHero from "../components/MiniHero";
 import Container from "../components/Container";
 import Row from "../components/Row";
@@ -7,119 +7,111 @@ import heroBackground from "../assets/hero-background.jpg";
 import axios from "axios";
 import moment from "moment";
 
-
-
-
-
-
 ///ARRAY OF SELECTED FRUIT TO DISPLAY
-let fruits = ["apple", "grapes", "berries", "banana", "apricot", "orange", "olive" ];
+let fruits = [
+  "apple",
+  "grapes",
+  "berries",
+  "banana",
+  "apricot",
+  "orange",
+  "olive",
+];
 
 //RANDOMLY SELECTING FRUIT OF THE DAY FROM THE ARRAY
 let rand = Math.random();
 let fruitsLength = fruits.length;
-let randIndex = Math.floor(rand * fruitsLength)
-let randomFruit = fruits[randIndex]
- 
-
-
-
+let randIndex = Math.floor(rand * fruitsLength);
+let randomFruit = fruits[randIndex];
 
 //// QUERY URL/PARAMETER FOR THE EDAMAM API
-const baseUrl = 'https://api.edamam.com/api/recipes/v2?type=public&'
-const initialQ = `q=${randomFruit}&app_id=5f2a8aa2&app_key=36918ebfaad4deeaf3f3cbc14048f581`
-
+const baseUrl = "https://api.edamam.com/api/recipes/v2?type=public&";
+const initialQ = `q=${randomFruit}&app_id=5f2a8aa2&app_key=36918ebfaad4deeaf3f3cbc14048f581`;
 
 function Eat() {
-
-
- const [day, setDay] = useState()
+  const [day, setDay] = useState();
   const [drink, setDrink] = useState([]);
   const [ingredients, setIngredients] = useState([]);
   const [meaiType, SetMealType] = useState([]);
   const [moreInfo, setMoreInfo] = useState();
   const [label, setLAbel] = useState();
-  const [calories, setCalories]= useState();
-  
+  const [calories, setCalories] = useState();
 
-  
-
-  
   //// GET THE DAY OF THE WEEK
-  useEffect(() =>{
-
-    setDay(moment().format(" dddd "))
-
-  },[setDay])
- 
+  useEffect(() => {
+    setDay(moment().format(" dddd "));
+  }, [setDay]);
 
   //// SETTING VALUES FROM EDAMAN API
-  useEffect (() => {
-    axios.get(baseUrl + initialQ)
-    .then((response)=> {
-
-      console.log(response)
-      setDrink(response.data.hits[0].recipe.images.REGULAR.url)
-      setIngredients(response.data.hits[0].recipe.ingredientLines)
-      SetMealType(response.data.hits[0].recipe.mealType)
-      setMoreInfo(response.data.hits[0].recipe.url)
-      setLAbel((response.data.hits[0].recipe.label))
-      setCalories((response.data.hits[0].recipe.calories))
-
-    })
-    
-
-  }, [])
-
-
-
-
-
+  useEffect(() => {
+    axios.get(baseUrl + initialQ).then((response) => {
+      console.log(response);
+      setDrink(response.data.hits[0].recipe.images.REGULAR.url);
+      setIngredients(response.data.hits[0].recipe.ingredientLines);
+      SetMealType(response.data.hits[0].recipe.mealType);
+      setMoreInfo(response.data.hits[0].recipe.url);
+      setLAbel(response.data.hits[0].recipe.label);
+      setCalories(response.data.hits[0].recipe.calories);
+    });
+  }, []);
 
   return (
-        <div className="mainContainer">
-    <div>
-      <MiniHero backgroundImage={heroBackground}>
-        <h1 className="pillarTitleName">Eat</h1>
-      </MiniHero>
-      <Container style={{ marginTop: 30 }}>
-      
-        <Row>
-          <Col size="md-12">
-            <h1>Your {day} Recipe</h1>
-           <h2>{label}</h2> 
-  
-        
-            <div>
-            <img src={drink} className="drinkImage"/>
-             
-            </div>
+    <div className="mainContainer">
+      <div>
+        <MiniHero backgroundImage={heroBackground}>
+          <h1 className="pillarTitleName">Eat</h1>
+        </MiniHero>
+        <Container style={{ marginTop: 30 }}>
+          <Row>
+            <Col size="md-12">
+              <h1>Your {day} Recipe</h1>
+              <h2>{label}</h2>
 
-            <div>
-              <h3 className="mainBodyText">Ingredients</h3>
+              <div>
+                <img src={drink} className="drinkImage" alt="drink" />
+              </div>
 
-              <p className="mainBodyText">{ingredients.map((i)=> (<li> {i}</li>))}</p>
+              <div>
+                <h3 className="mainBodyText">Ingredients</h3>
 
-            </div>
+                <p className="mainBodyText">
+                  {ingredients.map((i) => (
+                    <li> {i}</li>
+                  ))}
+                </p>
+              </div>
 
-            <div>
-            <br></br>
-              <h3 className="mainBodyText">Meal Type</h3>
-              <p className="mainBodyText">{meaiType.map((m)=> (<li>{m}</li>))} </p> 
-              <p className="mainBodyText"><li> calories : {" "}{calories} </li></p>
-              
-            </div>
+              <div>
+                <br></br>
+                <h3 className="mainBodyText">Meal Type</h3>
+                <p className="mainBodyText">
+                  {meaiType.map((m) => (
+                    <li>{m}</li>
+                  ))}{" "}
+                </p>
+                <p className="mainBodyText">
+                  <li> calories : {calories} </li>
+                </p>
+              </div>
 
-            <div>
-             <br></br>
-               <h3 className="mainBodyText"><a href={moreInfo} target='_blank' className="moreInfoRecipe">Click for more details</a></h3>
-                
-            </div>
-
-          </Col>
-        </Row>
-      </Container>
-    </div>
+              <div>
+                <br></br>
+                <h3 className="mainBodyText">
+                  <a
+                    href={moreInfo}
+                    target="_blank"
+                    className="moreInfoRecipe"
+                    rel="noreferrer"
+                    alt="moreInfo"
+                  >
+                    Click for more details
+                  </a>
+                </h3>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
